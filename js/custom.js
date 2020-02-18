f9f8def0392721cad267449037c396031aff1dd8
eliasemon <019eliashossain@gmail.com>
2020-02-18 16:00:33 +0600

    watter rapple remover and partical add

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,2 @@
-$(document).ready(function(){
-     $('#banner').ripples({
-        dropRadius: 10,
-        perturbance: 0.04
-     });
-     $('.toggle').click(function(){
-     	$('.menu').toggle(1000)
-     });
-});
+
   
